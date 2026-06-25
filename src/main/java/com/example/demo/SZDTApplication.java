87aea008ab34d0e4ed0c65ef86cb72b7de4abce7
Vuk Sekulic <vukosek@gmail.com>
2026-06-26 00:13:02 +0200

    Initial Commit

--- src/main/java/com/example/demo/SZDTApplication.java
+++ src/main/java/com/example/demo/SZDTApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SZDTApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SZDTApplication.class, args);
 	}
 
 }
